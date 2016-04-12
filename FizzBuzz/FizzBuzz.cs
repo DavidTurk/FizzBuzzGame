8fd82ca750450c8239c1265bfda9e6e1643b490d
David Turk <dturk@paraport.com>
2016-04-12 13:19:38 -0700

    TDD FizzBuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzzGame
+{
+    public class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static List<String> GetFizzBuzzList(int ceiling)
+        {
+            var fizzBuzzNumbers = new List<String>();
+
+            for (int i = 1; i <= ceiling; i++)
+            {
+                fizzBuzzNumbers.Add(GetNextNumber(i));
+            }
+
+            return fizzBuzzNumbers;
+        }
+
+        private static String GetNextNumber(int i)
+        {
+            bool divisibleByThree = ((i % 3) == 0);
+            bool divisibleByFive = ((i % 5 == 0));
+
+            if (divisibleByThree && divisibleByFive)
+            {
+                return "FizzBuzz";
+            }
+            else if (divisibleByFive)
+            {
+                return "Buzz";
+            }
+            else if (divisibleByThree)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return i.ToString();
+            }
+        }
+    }
+
+}
---
+++ TestFizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using FizzBuzzGame;
+using Shouldly;
+
+namespace TestFizzBuzzGame
+{
+    [TestFixture]
+    public class TestFizzBuzz
+    {
+        [Test]
+        public void TestFirstTwoNumbers()
+        {
+            var expectedList = new List<String> { "1", "2" };
+            var actualList = FizzBuzz.GetFizzBuzzList(2);
+
+            actualList.ShouldBe(expectedList);
+        }
+
+        [Test]
+        public void TestThrees()
+        {
+            var expectedList = new List<String> { "1", "2", "Fizz" };
+            var actualList = FizzBuzz.GetFizzBuzzList(3);
+
+            actualList.ShouldBe(expectedList);
+        }
+
+        [Test]
+        public void TestFives()
+        {
+            var expectedList = new List<String> { "1", "2", "Fizz", "4", "Buzz" };
+            var actualList = FizzBuzz.GetFizzBuzzList(5);
+
+            actualList.ShouldBe(expectedList);
+        }
+
+        [Test]
+        public void TestThreeandFive()
+        {
+            var testElement = 15;
+
+            var expectedValue = "FizzBuzz";
+            var actualList = FizzBuzz.GetFizzBuzzList(testElement);
+            var actualValue = actualList.ElementAt(testElement - 1);
+
+            actualValue.ShouldBe(expectedValue);
+        }
+    }
+}
+
+
